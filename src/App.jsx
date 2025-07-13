4be537b5a9df796a80d2e9132d12f771134f20e0
Aisamuddin <aisam0094@gmail.com>
2025-07-13 12:53:09 +0800

    feat: update api url to use from env

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 function App() {
